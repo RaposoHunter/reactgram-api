d7d18adb8181e6ab6a3e0d91815356895a1a13f9
Pedro Raposo <55608596+RaposoHunter@users.noreply.github.com>
2024-04-23 12:27:09 -0300

    :sparkles: middleware p/ upload de imagens

---
+++ middlewares/ImageUpload.ts
@@ -0,0 +1,32 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let folder = '';
+
+        if(req.baseUrl.includes('users')) {
+            folder = 'users';
+        } else if(req.baseUrl.includes('photos')) {
+            folder = 'photos';
+        }
+
+        cb(null, `uploads/${folder}`);
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ 
+    storage,
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(png|jpg|jpeg|webp)$/)) {
+            return cb(new Error('Extensão inválida'));
+        }
+
+        cb(null, true);
+    }
+});
+
+export default upload;
